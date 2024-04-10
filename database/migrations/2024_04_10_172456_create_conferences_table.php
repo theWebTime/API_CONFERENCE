ad3de0c345398b67826f886daac70e030b3f716e
Ayush <ayushchhaya25@gmail.com>
2024-04-11 00:27:13 +0530

    unsaved fil saved

--- database/migrations/2024_04_10_172456_create_conferences_table.php
+++ database/migrations/2024_04_10_172456_create_conferences_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('conferences', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('domain', 50)->unique();
             $table->string('title', 50)->unique();
             $table->string('date', 30);
